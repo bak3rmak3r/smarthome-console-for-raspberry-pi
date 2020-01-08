d9787bcf05d9504f2e7196114470160e257bc22e
Mark Becker <59669052+bak3rmak3r@users.noreply.github.com>
2020-01-08 23:46:46 +0100

    comment

--- web/start_smarthome.sh
+++ web/start_smarthome.sh
@@ -1,1 +1,2 @@
+#open openhab2 dashboard or other dashboard in chromium on touch display
 DISPLAY=:0 chromium-browser --fast --kiosk --noerrordialogs --disable-session-crashed-bubble 'http://IP:8080/habpanel/index.html#/view/MainDash' &
