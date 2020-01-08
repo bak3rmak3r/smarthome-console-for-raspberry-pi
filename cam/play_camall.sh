da51c76ee0af03b1448e7ec0b8dce86d37ed84c6
Mark Becker <59669052+bak3rmak3r@users.noreply.github.com>
2020-01-08 23:48:27 +0100

    comment

--- cam/play_camall.sh
+++ cam/play_camall.sh
@@ -1,3 +1,4 @@
+#play available streams, up to 4 or depending on resolution adjustments, as overview on touch screen
 sh /opt/automation/cam/play_cam1.sh &
 sh /opt/automation/cam/play_cam2.sh &
 sh /opt/automation/cam/play_cam3.sh &
